284b2952cd8b24a724515efa6e67665584c7b128
Sagi-o <sagioshri@gmail.com>
2021-12-30 11:15:21 +0200

    added jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,23 +3,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './models/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './models/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
 
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.usersRepository.findOne({ username });
+    const isPasswordsMatch = await bcrypt.compare(password, user?.password);
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      return 'Success';
+    if (user && isPasswordsMatch) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = this.jwtService.sign(payload);
+      return { accessToken };
     }
 
     throw new UnauthorizedException('Please check in your logic credentials');
